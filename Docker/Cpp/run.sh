1d80334921b799aa0d2cff12524997579880cb33
Liming Jin <jinliming2@gmail.com>
2016-11-22 14:58:40 +0800

    Enhance compile process. Add 'EasySandbox' module.🌻

--- Docker/Cpp/run.sh
+++ Docker/Cpp/run.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-timeout 30s g++ /mnt/main.cpp -static -o ~/jail/main.out
+timeout 30s g++ /mnt/main.cpp -o ~/jail/main.out -ldl /jail.so
 rc=$?
 if [ ${rc} -eq 124 ]; then
     echo "Compile Time Out"
